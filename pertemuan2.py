06539d391d41452d30f853e9b0e9bf6bb09475f7
Antung Hissyam <hissyam08@gmail.com>
2025-09-16 10:23:48 +0800

    Testing (3)

--- pertemuan2.py
+++ pertemuan2.py
@@ -11,3 +11,34 @@ print("Praktikum APD Pertemuan 2")
 print(string)
 #cek tipe data
 print(type(string))
+
+print("""
+      biaya
+      parkir = """
+      )
+nama = "Antung Hissyam"
+print("Nama saya adalah " + nama)
+teks1 = "ryo yamada istri aku"
+print(teks1[0:3])
+
+hujan = False
+
+if hujan:
+    print("bawa payung")
+else:
+    print("tidak usah bawa payung")
+
+a = ['pdiddy', 'w speed', 'capek', 'ryo yamada istri ku']
+print (a[0])
+print(a[1])
+print(a[2])
+a[3] = 'ryo yamada my bubub'
+print(a[3])
+
+warna = {
+'warna1' : 'merah',
+'warna2' : 'kuning',
+'warna3' : 'hijau'
+}
+print(warna['warna1'])
+print(warna['warna3'])
