b9c763f6d2668e9f50b34408cb12aa04c17ba05e
Antung Hissyam <hissyam08@gmail.com>
2025-09-16 10:05:23 +0800

    Testing (2)

--- pertemuan2.py
+++ pertemuan2.py
@@ -1,7 +1,13 @@
 angkaInteger = 10
 angkaFloat = 5.5
+string = 'Testing tanda petik tunggal'
 print(angkaInteger)
 print(angkaFloat)
-
+#cek tipe data
 print(type(angkaInteger))
 print(type(angkaFloat))
+
+print("Praktikum APD Pertemuan 2")
+print(string)
+#cek tipe data
+print(type(string))
